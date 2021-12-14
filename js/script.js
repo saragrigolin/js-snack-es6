6842477354e5ee9479c7e0b47ce7431cced325f6
saragrigolin <56165909+saragrigolin@users.noreply.github.com>
2021-12-14 10:21:04 +0100

    script with filter

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ Usiamo i nuovi metodi degli array foreach o filter.
 let arrayNames = ['Sara', 'Matilde', 'Massimo', 'Cristiana', 'Eleonora', 'Francesco'];
 
 let a = 2;
-let b = 6;
+let b = 5;
 
 console.log('Array originale: '+ arrayNames);
 console.log('a: ' + a + ' , b: ' + b);
@@ -16,13 +16,22 @@ console.log('a: ' + a + ' , b: ' + b);
 let arrayNew = getNames(arrayNames, a, b);
 console.log('Array nuovo: ' + arrayNew);
 
-//funzione per ottenere i valori dell'array che hanno la posizione compresa tra i due numeri
-function getNames(array, num1, num2){
-    let arrayNew = [];
-    array.forEach((element, index) => {
-        if (index >= num1 && index <= num2){
-            arrayNew.push(element);
-        }
+//funzione foreach per ottenere i valori dell'array che hanno la posizione compresa tra i due numeri
+
+// function getNames(array, num1, num2){
+//     let arrayNew = [];
+//     array.forEach((element, index) => {
+//         if (index >= num1 && index <= num2){
+//             arrayNew.push(element);
+//         }
+//     });
+//     return arrayNew;
+// }
+
+//funzione filter
+function getNames(array, num1, num2) {
+    let arrayNew = array.filter((element, index) => {
+        return index >= num1 && index <= num2;
     });
     return arrayNew;
 }
